7d10d54b958caa1c6f71fa87873ebec4d8f65ec3
Vasanth Developer <vsnthdev@gmail.com>
2022-01-09 18:14:52 +0000

    ✨ logname variable is now available to config files

--- src/util/systemInfo.ts
+++ src/util/systemInfo.ts
@@ -4,16 +4,35 @@
  */
 
 import si from 'systeminformation'
+import { exec } from 'child_process'
+
+const exe = (cmd: string): Promise<string> =>
+    new Promise((resolve, reject) => {
+        exec(cmd, (err, stdout) => {
+            if (err) {
+                reject(err)
+            } else {
+                resolve(stdout.trim())
+            }
+        })
+    })
 
 export default async () => {
     const info = {
-        os: await si.osInfo(),
+        os: {
+            ...(await si.osInfo()),
+            ...{
+                logname: await exe('logname'),
+            },
+        },
         cpu: await si.cpu(),
         system: await si.system(),
     }
 
     // remove some sensitive fields
     delete info.cpu.flags
+
+    console.log(info.os)
 
     return info
 }
